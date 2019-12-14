be6301d6c35c525216aa5935ae74e80c3249be88
Jonathan Gan <jonathang132298@gmail.com>
2019-12-14 16:20:49 -0500

    make it better

--- src/Components/About.js
+++ src/Components/About.js
@@ -24,7 +24,14 @@ class About extends Component {
          </div>
          <div className="nine columns main-col">
 
-<iframe width="300" height="200" src="https://www.youtube.com/embed/ELSdieLPV70" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe 
+width="300" 
+height="200" 
+src="https://www.youtube.com/embed/ELSdieLPV70" 
+frameborder="0" 
+allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+allowfullscreen>
+</iframe>
 
             <h2>About Me</h2>
             
